d82852cd7fc95097f19828cfea2435a5479caca5
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-03-01 23:09:01 -0500

    arrays

---
+++ 02Basics/arrays.ts
@@ -0,0 +1,20 @@
+const superHeroes: string[] = [];
+// const heroPowers: number[] = [];
+const heroPowers: Array<number> = [];
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUsers: User[] = [];
+
+const MLModels: number[][] = [
+  [255, 255, 255],
+  [1, 1, 1],
+];
+
+superHeroes.push("spiderman");
+heroPowers.push(8);
+
+allUsers.push({ name: "durpin", isActive: true });
