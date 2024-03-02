8648557465456cff0fc65ae1b125972af7021973
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-03-02 18:50:31 -0500

    enums in ts

---
+++ 02Basics/enum.ts
@@ -0,0 +1,17 @@
+// const AISLE = 0;
+// const MIDDLE = 1;
+// const WINDOW = 2;
+
+enum SeatChoice {
+  AISLE, //= 10,
+  MIDDLE,
+  WINDOW,
+}
+
+const enum AirSeatChoice {
+  AISLE,
+  MIDDLE,
+  WINDOW,
+}
+
+const hcSeat = SeatChoice.WINDOW;
