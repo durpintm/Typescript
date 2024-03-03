1c795caba5a799a3684159286c9ebce43bdc2268
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-03-02 19:36:22 -0500

    new project ts

---
+++ 03PureTs/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("Typescript is here");
+// tsc -w => for watch mode in ts
+const hello = "hey there";
---
+++ 03PureTs/src/index.ts
@@ -0,0 +1,4 @@
+console.log("Typescript is here");
+// tsc -w => for watch mode in ts
+
+const hello: string = "hey there";
