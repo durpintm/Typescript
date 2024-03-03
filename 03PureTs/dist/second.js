8d00f57c2958eeab4c001d5bd5780cbb776c11f4
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-03-02 20:12:27 -0500

    implements in ts

---
+++ 03PureTs/dist/second.js
@@ -0,0 +1,18 @@
+"use strict";
+class Instagram {
+    constructor(cameraMode, filter, burstMode) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burstMode = burstMode;
+    }
+}
+class Youtube {
+    constructor(cameraMode, filter, burstMode) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burstMode = burstMode;
+    }
+    createStory() {
+        console.log("Story was created!");
+    }
+}
---
+++ 03PureTs/src/second.ts
@@ -0,0 +1,28 @@
+interface TakePhoto {
+  cameraMode: string;
+  filter: string;
+  burstMode: number;
+}
+
+class Instagram implements TakePhoto {
+  constructor(
+    public cameraMode: string,
+    public filter: string,
+    public burstMode: number
+  ) {}
+}
+
+class Youtube implements TakePhoto, Story {
+  constructor(
+    public cameraMode: string,
+    public filter: string,
+    public burstMode: number
+  ) {}
+  createStory(): void {
+    console.log("Story was created!");
+  }
+}
+
+interface Story {
+  createStory(): void;
+}
